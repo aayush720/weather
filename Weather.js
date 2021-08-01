c2c828122136f5212356078b506579c5a0c131e1
aayush720 <46783470+aayush720@users.noreply.github.com>
2021-08-01 18:38:23 +0530

    Add files via upload

---
+++ Weather.js
@@ -0,0 +1,5 @@
+async function funcName(){
+	url= "https://run.mocky.io/v3/e3ae9d2e-78f5-403d-b6cd-fa7f8c7e1576"
+    const response = await fetch(url);
+    var data = await response.json();
+    }
